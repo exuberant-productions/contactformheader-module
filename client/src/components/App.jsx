d8e0d5035220d85b15cf4550d3f6abf480dd6dad
doodoodoom <madridmatt17@gmail.com>
2019-02-13 04:45:57 -0800

    Mockup header, implement PostGrotesk font

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Header from './Header.jsx';
+import Header from './Header.jsx';
 import Contactform from './Contactform.jsx';
 
 class App extends React.Component {
@@ -13,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Contactform />
+        <Header />
       </div>
     );
   }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,9 +10,35 @@ class Header extends React.Component {
 
   render() {
     return (
-      <h1>
-        THIS IS THE HEADER LOL
-      </h1>
+      <div name="header">
+        <div name="location">
+          <h1 name="address">
+            2145 California St #5
+          </h1>
+          <h2 name="cityStateEtc">
+            San Francisco, CA 94115 (Pacific Heights)
+          </h2>
+        </div>
+        <div name="pricePoint">
+          <h1 name="price">
+            $595,000
+          </h1>
+          <h2 name="mortgage">
+            Est. Mortgage $3,255/mo
+          </h2>
+          <button name="preQualify">
+            Get Pre-Qualified
+          </button>
+        </div>
+        <div name="formDropdown">
+          <h1 name="fakeButton">
+            Request Info
+          </h1>
+          <h2 name="downArrow">
+            &#8964;
+          </h2>
+        </div>
+      </div>
     );
   }
 }
