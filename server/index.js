bd248df3bb0a2a9ff20a8ad31e390922a8058e37
doodoodoom <madridmatt17@gmail.com>
2019-02-19 19:52:03 -0800

    Enable server communication with MySQL database

--- server/index.js
+++ server/index.js
@@ -4,36 +4,65 @@ const app = express();
 const port = 3003;
 const cors = require('cors');
 
-const { getHomeInfo, getAgentInfo, getContactFormInfo } = require('../database/index.js');
+const { getHomeInfo, getListingAgentInfo, getPremierAgent1Info, getPremierAgent2Info, getPremierAgent3Info, postContactFormInfo } = require('../database/index.js');
 
 app.use(cors());
 
+app.use(express.json());
+
 app.use(express.static(`${__dirname}/../public`));
 
-app.get('/api/homeinfo', (req, res) => {
-  getHomeInfo((err, data) => {
+app.get('/api/homeinfo/:homeID', (req, res) => {
+  getHomeInfo(req.params.homeID, (err, data) => {
     if (err) {
       throw ('ERROR AT HOMEINFO GET', err);
     }
-    res.send(data);
+    res.send(data[0]);
   });
 });
 
-app.get('/api/agentinfo', (req, res) => {
-  getAgentInfo((err, data) => {
+app.get('/api/homeinfo/:homeID/listingagentinfo', (req, res) => {
+  getListingAgentInfo(req.params.homeID, (err, data) => {
     if (err) {
       throw ('ERROR AT AGENTINFO GET', err);
     }
-    res.send(data);
+    res.send(data[0]);
   });
 });
 
-app.get('/api/contactforminfo', (req, res) => {
-  getContactFormInfo((err, data) => {
+app.get('/api/homeinfo/:homeID/premieragent1info', (req, res) => {
+  getPremierAgent1Info(req.params.homeID, (err, data) => {
     if (err) {
-      throw ('ERROR AT CONTACTFORMINFO GET', err);
+      throw ('ERROR AT AGENTINFO GET', err);
     }
-    res.send(data);
+    res.send(data[0]);
+  });
+});
+
+app.get('/api/homeinfo/:homeID/premieragent2info', (req, res) => {
+  getPremierAgent2Info(req.params.homeID, (err, data) => {
+    if (err) {
+      throw ('ERROR AT AGENTINFO GET', err);
+    }
+    res.send(data[0]);
+  });
+});
+
+app.get('/api/homeinfo/:homeID/premieragent3info', (req, res) => {
+  getPremierAgent3Info(req.params.homeID, (err, data) => {
+    if (err) {
+      throw ('ERROR AT AGENTINFO GET', err);
+    }
+    res.send(data[0]);
+  });
+});
+
+app.post('/api/contactforminfo', (req, res) => {
+  postContactFormInfo(req.body, (err) => {
+    if (err) {
+      throw ('ERROR AT CONTACTFORMINFO POST', err);
+    }
+    res.sendStatus(201);
   });
 });
 
