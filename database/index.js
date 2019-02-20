b4145b2d76b50dd9ec19c6371390bf161ba118db
doodoodoom <madridmatt17@gmail.com>
2019-02-19 19:50:52 -0800

    Implement SQL query methods

--- database/index.js
+++ database/index.js
@@ -9,8 +9,8 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-const getHomeInfo = (callback) => {
-  connection.query('SELECT * FROM home_info', (err, rows) => {
+const getHomeInfo = (homeID, callback) => {
+  connection.query(`SELECT * FROM home_info WHERE id = ${homeID}`, (err, rows) => {
     if (err) {
       throw ('ERROR AT home_info REQUEST', err);
     }
@@ -18,24 +18,56 @@ const getHomeInfo = (callback) => {
   });
 };
 
-const getAgentInfo = (callback) => {
-  connection.query('SELECT * FROM agent_info', (err, rows) => {
+const getListingAgentInfo = (homeID, callback) => {
+  connection.query(`SELECT * FROM agent_info WHERE id=(SELECT listing_agent FROM home_info WHERE id = ${homeID})`, (err, rows) => {
     if (err) {
-      throw ('ERROR AT agnet_info REQUEST', err);
+      throw ('ERROR AT agent_info REQUEST (LISTING AGENT)', err);
     }
     callback(null, rows);
   });
 };
 
-const getContactFormInfo = (callback) => {
-  connection.query('SELECT * FROM contact_form_info', (err, rows) => {
+const getPremierAgent1Info = (homeID, callback) => {
+  connection.query(`SELECT * FROM agent_info WHERE id=(SELECT premier_agent1 FROM home_info WHERE id = ${homeID})`, (err, rows) => {
     if (err) {
-      throw ('ERROR AT contact_form_info REQUEST', err);
+      throw ('ERROR AT agent_info REQUEST (PREMIER AGENT 1)', err);
     }
     callback(null, rows);
+  });
+};
+
+const getPremierAgent2Info = (homeID, callback) => {
+  connection.query(`SELECT * FROM agent_info WHERE id=(SELECT premier_agent2 FROM home_info WHERE id = ${homeID})`, (err, rows) => {
+    if (err) {
+      throw ('ERROR AT agent_info REQUEST (PREMIER AGENT 2)', err);
+    }
+    callback(null, rows);
+  });
+};
+
+const getPremierAgent3Info = (homeID, callback) => {
+  connection.query(`SELECT * FROM agent_info WHERE id=(SELECT premier_agent3 FROM home_info WHERE id = ${homeID})`, (err, rows) => {
+    if (err) {
+      throw ('ERROR AT agent_info REQUEST (PREMIER AGENT 3)', err);
+    }
+    callback(null, rows);
+  });
+};
+
+const postContactFormInfo = (data, callback) => {
+  connection.query(`INSERT INTO contact_form_info (name, email, phone_number, message, is_into_financing, property_id)
+    VALUES ('${data.name}', '${data.email}', '${data.phone}', '${data.message}', ${data.isIntoFinancing}, ${data.propertyID})`, 
+    (err) => {
+      if (err) {
+        throw ('ERROR AT contact_form_info REQUEST', err);
+      }
+    callback(null);
   });
 };
 
 module.exports.getHomeInfo = getHomeInfo;
-module.exports.getAgentInfo = getAgentInfo;
-module.exports.getContactFormInfo = getContactFormInfo;
+module.exports.getListingAgentInfo = getListingAgentInfo;
+module.exports.getPremierAgent1Info = getPremierAgent1Info;
+module.exports.getPremierAgent2Info = getPremierAgent2Info;
+module.exports.getPremierAgent3Info = getPremierAgent3Info;
+module.exports.postContactFormInfo = postContactFormInfo;
